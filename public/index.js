9687a239df5ad8ee7ba4a77276b7ea724a38aa56
LukaVekua <luka.vekua@gau.edu.ge>
2021-01-23 15:34:06 +0400

    shsdhsdhsd

---
+++ public/index.js
@@ -0,0 +1,25 @@
+const imgModal = document.getElementsByClassName('image-modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close');
+const popup = document.querySelector('.popup');
+
+
+for(let i =0; i<13; i++) {
+
+
+
+    imgModal[i].addEventListener('click', () => {
+        overlay.classList.add('active');
+        popup.innerHTML = `<img src="` + imgModal[i].src + `">`;
+    })
+
+    close.addEventListener('click', () => {
+        overlay.classList.remove('active');
+    })
+
+}
+
+
+
+
+
